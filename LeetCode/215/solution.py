3e087932ce88c6bc07b7eb66aedfe8aa758f479e
adore_blvnk <ekhlypse13@gmail.com>
2022-03-24 09:52:41 +0800

    fix(881): change to 215

--- LeetCode/215/solution.py
+++ LeetCode/215/solution.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse=True)[k-1]
+        return sorted(nums)[-k]
 
 
 print(Solution.findKthLargest("", [3, 2, 1, 5, 6, 4], 2))
