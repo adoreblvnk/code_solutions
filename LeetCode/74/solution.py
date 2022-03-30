a40d16acf988c6b28e3cd82d149c99f88d97a392
adore_blvnk <ekhlypse13@gmail.com>
2022-03-30 09:44:17 +0800

    feat(): 74

---
+++ LeetCode/74/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in matrix:
+            if target in i:
+                return True
+        return False
+
+print(Solution.searchMatrix("", [[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
