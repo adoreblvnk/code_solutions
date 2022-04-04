c83a357cc7ca433db694fb95d38df9d3bb2c3ae4
adore_blvnk <ekhlypse13@gmail.com>
2022-04-04 16:13:59 +0800

    feat(): 14

---
+++ LeetCode/14/solution.py
@@ -0,0 +1,16 @@
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        for i, ch in enumerate(min(strs, key=len)):
+            for other in strs:
+                if other[i] != ch:
+                    return strs[0][:i]
+        return min(strs, key=len)
+
+
+print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
