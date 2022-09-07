e34044cbb69d22e71d14f3cb5e933af1f23edbb3
adore_blvnk <83258252+adoreblvnk@users.noreply.github.com>
2022-09-07 11:20:15 +0800

    feat(go): valid parentheses

---
+++ LeetCode_Go/valid_parentheses.go
@@ -0,0 +1,33 @@
+// 20 Valid Parentheses
+// Results:
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Valid Parentheses.
+// Memory Usage: 2.1 MB, less than 45.81% of Go online submissions for Valid Parentheses.
+// https://leetcode.com/submissions/detail/793527191/
+
+package main
+
+import "fmt"
+
+// check top element of stack.
+func isValid(s string) bool {
+	bracketPair := map[rune]rune{'(': ')', '[': ']', '{': '}'}
+	stack := []rune{}
+	for _, val := range s {
+		switch val {
+		case '(', '[', '{':
+			stack = append(stack, bracketPair[val])
+		default:
+			if len(stack) == 0 || stack[len(stack)-1] != val {
+				return false
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	s := "]"
+	fmt.Println(isValid(s))
+}
