beb32fdff0886f0330d110ac4fbcc9e0144da0d5
adore_blvnk <83258252+adoreblvnk@users.noreply.github.com>
2022-10-08 11:32:56 +0800

    feat(cpp): plus one

---
+++ cpp_LeetCode/plus_one.cpp
@@ -0,0 +1,68 @@
+// Plus One
+// Results:
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Plus One.
+// Memory Usage: 8.6 MB, less than 98.33% of C++ online submissions for Plus One.
+// https://leetcode.com/submissions/detail/817592395/
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using std::cout, std::endl, std::string, std::vector, std::map;
+
+// overloads operator << to print formatted vectors.
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const vector<T> vec) {
+    os << "[";
+    if (vec.empty()) {
+        return os << "]";
+    }
+    for (int i{0}; i < vec.size(); i++) {
+        os << vec.at(i);
+        if (i != vec.size() - 1) {
+            os << ", ";
+        }
+    }
+    return os << "]";
+}
+
+// overloads operator << to print formatted maps.
+template <typename T, typename T1>
+std::ostream& operator<<(std::ostream& os, const map<T, T1> dict) {
+    os << "{";
+    if (dict.empty()) {
+        return os << "}";
+    }
+    int i{0};
+    for (auto& [key, value] : dict) {
+        os << key << ": " << value;
+        if (i < dict.size() - 1) {
+            os << ", ";
+        }
+        i++;
+    }
+    return os << "}";
+}
+
+class Solution {
+   public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+int main() {
+    vector<int> digits{9};
+    cout << Solution().plusOne(digits) << endl;
+    return 0;
+}
